d12837594587a4e7240201add85795a2d1b22d22
Edward Hietter <ethietter@gmail.com>
2016-02-04 13:53:14 -0700

    Created the catkin workspace and an initial package called 'ipp'

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ethietter/fiore-ipp/ipp-15-16/catkin_ws/src
+# Build directory: /home/ethietter/fiore-ipp/ipp-15-16/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(ipp)
---
+++ catkin_ws/build/ipp/catkin_generated/pkg.installspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
+PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "ipp"
+PROJECT_SPACE_DIR = "/home/ethietter/fiore-ipp/ipp-15-16/catkin_ws/install"
+PROJECT_VERSION = "0.0.0"
