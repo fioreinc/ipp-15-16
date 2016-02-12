1d5b8c1407f32a811749a947af0625e7d1a49253
Eric <ewats13@unm.edu>
2016-02-12 13:07:22 -0700

    ros modifications

--- catkin_ws/src/loadcell_calibration/scripts/loadcell.py
+++ catkin_ws/src/loadcell_calibration/scripts/loadcell.py
@@ -5,17 +5,34 @@ import rospy
 
 from loadcell_calibration.srv import *
 
-def init():
+def loadcell_init_client():
     rospy.wait_for_service('get_factor')
     try:
         get_factor = rospy.ServiceProxy('get_factor', GetFactor);
-        lbs = 100
-        factor = 2
-        for x in range(0, 10):
-            response = get_factor(lbs*factor)
-            factor = response.factor
+#        lbs = 100
+#        factor = 2
+#        for x in range(0, 10):
+#            response = get_factor(lbs*factor)
+#            factor = response.factor
+	response = get_factor(x)
+	return response.is_done
+
+
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
 
+def usage():
+	return "%s [x]"%sys.argv[0]
+
 if __name__ == "__main__":
-    init();
+
+	if len(sys.argv) == 2:
+		x = float(sys.argv[1])
+	else:
+		print usage()
+		sys.exit(1)
+
+	print "Inputted Calibration Weight: %s"%(loadcell_init_client(x))
+
+
+#! then pass this weight to calibration factor calculation
