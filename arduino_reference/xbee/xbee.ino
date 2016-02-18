81a4245e2ff324d4d8801dc725ab7b7ed228374b
Edward Hietter <ethietter@gmail.com>
2016-02-18 15:06:18 -0700

    Created a proof of concept sketch demonstrating how to read data from the Xbee

---
+++ arduino_reference/xbee/xbee.ino
@@ -0,0 +1,47 @@
+//#include <SoftwareSerial.h>
+
+//SoftwareSerial XBee(2, 3); // RX, TX
+
+int data_received = 0;
+void setup()
+{
+    // Set up both ports at 9600 baud. This value is most important
+    // for the XBee. Make sure the baud rate matches the config
+    // setting of your XBee.
+    //XBee.begin(9600);
+    Serial.begin(9600);
+    pinMode(13, OUTPUT);
+}
+
+void blink(){
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
+}
+
+char convertCase(char c){
+    if(c >= 'a' && c <= 'z'){
+        return c + ('A' - 'a');
+    }
+    else if(c >= 'A' && c <= 'Z'){
+        return c + ('a' - 'A');
+    }
+}
+
+void loop()
+{
+    char c;
+    while (Serial.available() > 0) {
+        c = Serial.read();
+        c = convertCase(c);
+        Serial.write(c);
+    }
+/*
+    if(data_received){
+        blink();
+        data_received = 0;
+    }
+*/
+}
+
