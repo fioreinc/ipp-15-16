15a082a5d4c7f790488bceea21b93fbb33a41531
Edward Hietter <ethietter@gmail.com>
2016-04-19 14:19:00 -0600

    Added some valid commands

--- arduino_flexiforce/motor_code/motor_code.ino
+++ arduino_flexiforce/motor_code/motor_code.ino
@@ -21,12 +21,158 @@ void readXBee(){
     }
 }
 
-float target[NUM_SERVOS] = {.5, .9, .9, .9, .9};
+//float target[NUM_SERVOS] = {.5, .9, .9, .9, .9};
 
 float target_voltage = .40;
 int reached_target_voltage = 0;
 
+bool allServosAtTarget(int result[NUM_SERVOS]){
+    for(int i = 0; i < NUM_SERVOS; i++){
+        if(result[i] == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+void reset(){
+    bool all_reached = false;
+    float target[NUM_SERVOS] = {0, 0, 0, 0, 0};
+    int result[NUM_SERVOS];
+    //while(!allServosAtTarget(result)){
+    while(1){
+        servo_control->gripTo(target, 2, result);
+        String cmd = "";
+        if(serial_comm->getCmdIfAvailable(cmd) != -1){
+            if(cmd == "exitreset"){
+                return;
+            }
+        }
+    }
+}
+
+void mario(){
+
+}
+
+bool checkReset(){
+    String cmd = "";
+    if(serial_comm->getCmdIfAvailable(cmd) == -1){
+        return false;
+    }
+
+    if(cmd == "reset"){
+        reset();
+        return true;
+    }
+
+    return false;
+}
+
+void openClose(){
+    int result[NUM_SERVOS];
+    int speed = 2;
+    float target[NUM_SERVOS] = {0, 0, 0, 0, 0};
+    while(1){
+        if(checkReset()){
+            return;
+        }
+        servo_control->gripTo(target, speed, result);
+        for(int i = 1; i < NUM_SERVOS; i++){
+            if(result[i]){
+                if(target[i] == 1){
+                    target[i] = 0;
+                }
+                else {
+                    target[i] = 1;
+                }
+            }
+        }
+    }
+}
+
+void gripCoffee(){
+    float voltages[NUM_SERVOS];
+    int result[NUM_SERVOS];
+    int speed = 2;
+    float target[NUM_SERVOS] = {.5, 1, 1, 1, 1};
+    float threshold = 1;
+    while(1){
+        if(checkReset()){
+            return;
+        }
+
+        sensor_control->readAll(voltages);
+        Serial.print("Thumb: ");
+        Serial.print(voltages[0]);
+        Serial.print("\nRing: ");
+        Serial.print(voltages[3]);
+        Serial.print("\n");
+        for(int i = 0; i < NUM_SENSORS; i++){
+            if(voltages[i] >= threshold){
+                Serial.print("Threshold reached on ");
+                Serial.print(i);
+                Serial.print(" Value=");
+                Serial.print(voltages[i]);
+                Serial.print("\n");
+                return;
+            }
+        }
+        servo_control->gripTo(target, speed, result);
+    }
+}
+
+void readSensors(){
+    while(1){
+        float voltages[NUM_SERVOS];
+        sensor_control->readAll(voltages);
+        Serial.print(voltages[3]);
+        Serial.print("\n");
+    }
+}
+
+
+
+
+void execCommand(String &cmd){
+    if(cmd == "openclose"){
+        openClose();
+    }
+    else if(cmd == "reset"){
+        reset();
+    }
+    else if(cmd == "gripcoffee"){
+        gripCoffee();
+    }
+    else if(cmd == "readsensors"){
+        readSensors();
+    }
+    else if(cmd == "Mario"){
+        mario();
+    }
+}
+
 void loop() {
+    String cmd;
+    if(serial_comm->getCmdIfAvailable(cmd) != -1){
+        execCommand(cmd);
+    }
+    return;
+/*
+        if(cmd == "stop"){
+            reached_target_voltage = 1;
+        }
+        if(cmd == "start"){
+            reached_target_voltage = 0;
+        }
+        if(cmd == "reset"){
+            while(1){
+                float target[NUM_SERVOS] = {.3, 0, 0, 0, 0};
+                int result[NUM_SERVOS];
+                servo_control->gripTo(target, 2, result);
+            }
+        }
+*/
     int speed = 1;
     //Read all of the sensors, and if any of them are > target voltage, stop
     float result[NUM_SENSORS];
@@ -53,6 +199,7 @@ void loop() {
     }
     */
 
+    /*
     String cmd;
     if(serial_comm->getCmdIfAvailable(cmd) != -1){
         if(cmd == "stop"){
@@ -69,11 +216,12 @@ void loop() {
             }
         }
     }
+    */
 
+        /*
     if(!reached_target_voltage){
         int result[NUM_SERVOS];
         servo_control->gripTo(target, speed, result);
-        /*
         for(int i = 1; i < NUM_SERVOS; i++){
             if(result[i]){
                 if(target[i] == .7){
@@ -84,6 +232,6 @@ void loop() {
                 }
             }
         }
-        */
     }
+        */
 }
