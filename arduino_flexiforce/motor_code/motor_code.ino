41da0fe38dcfd5d2a0f4bb7fe635e0017e9765c3
Edward Hietter <ethietter@gmail.com>
2016-04-22 15:12:23 -0600

    Finished testing.

--- arduino_flexiforce/motor_code/motor_code.ino
+++ arduino_flexiforce/motor_code/motor_code.ino
@@ -13,18 +13,6 @@ void setup() {
     sensor_control = new SensorControl();
 }
 
-void readXBee(){
-    char c;
-    while(Serial.available() > 0){
-        c = Serial.read();
-        Serial.write(c);
-    }
-}
-
-//float target[NUM_SERVOS] = {.5, .9, .9, .9, .9};
-
-float target_voltage = .40;
-int reached_target_voltage = 0;
 
 bool allServosAtTarget(int result[NUM_SERVOS]){
     for(int i = 0; i < NUM_SERVOS; i++){
@@ -52,7 +40,40 @@ void reset(){
 }
 
 void mario(){
+    int result[NUM_SERVOS];
+    int speed = 2;
+    float target[NUM_SERVOS] = {0, 1, 0, 1, 1};
+    while(1){
+        if(checkReset()){
+            return;
+        }
+        servo_control->gripTo(target, speed, result);
+    }
+}
 
+void lobo(){
+    int result[NUM_SERVOS];
+    int speed = 2;
+    float target[NUM_SERVOS] = {.3, 0, .5, .5, 0};
+    while(1){
+        if(checkReset()){
+            return;
+        }
+        servo_control->gripTo(target, speed, result);
+    }
+}
+
+void downFinger(int index){
+    int result[NUM_SERVOS];
+    int speed = 2;
+    float target[NUM_SERVOS] = {0, 0, 0, 0, 0};
+    target[index] = .8;
+    while(1){
+        if(checkReset()){
+            return;
+        }
+        servo_control->gripTo(target, speed, result);
+    }
 }
 
 bool checkReset(){
@@ -78,61 +99,120 @@ void openClose(){
             return;
         }
         servo_control->gripTo(target, speed, result);
-        for(int i = 1; i < NUM_SERVOS; i++){
+        for(int i = 0; i < NUM_SERVOS; i++){
             if(result[i]){
-                if(target[i] == 1){
+                if(target[i] == .8){
                     target[i] = 0;
                 }
                 else {
-                    target[i] = 1;
+                    target[i] = .8;
                 }
             }
         }
+        /*
+        if(result[0]){
+            if(target[0] == 0){
+                target[0] = .8;
+            }
+            else{
+                target[0] = 0;
+            }
+        }
+        */
     }
 }
 
-void gripCoffee(){
-    float voltages[NUM_SERVOS];
-    int result[NUM_SERVOS];
+void gripEraser(){//Good
     int speed = 2;
     float target[NUM_SERVOS] = {.5, 1, 1, 1, 1};
-    float threshold = 1;
+    float threshold = .8;
+    execTestCase(target, speed, threshold, "Grip Eraser");
+}
+
+void gripWater(){//Good
+    int speed = 2;
+    float target[NUM_SERVOS] = {.5, .8, .8, .8, .8};
+    float threshold = .8;
+    execTestCase(target, speed, threshold, "Grip Water");
+}
+
+void gripThermos(){
+    int speed = 2;
+    float target[NUM_SERVOS] = {.5, .6, .6, .6, .6};
+    float threshold = 1.4;
+    execTestCase(target, speed, threshold, "Grip Thermos");
+}
+
+void gripCoke(){//Good
+    int speed = 2;
+    float target[NUM_SERVOS] = {.55, .55, .55, .55, .55};
+    float threshold = 1.3;
+    execTestCase(target, speed, threshold, "Grip Coke");
+}
+
+void gripFullWater(){
+    int speed = 2;
+    float target[NUM_SERVOS] = {.55, .65, .65, .65, .65};
+    float threshold = 1.6;
+    execTestCase(target, speed, threshold, "Grip Coke");
+}
+
+void gripFullTea(){//Good
+    int speed = 2;
+    float target[NUM_SERVOS] = {.55, .55, .55, .55, .55};
+    float threshold = .8;
+    execTestCase(target, speed, threshold, "Grip Full Tea");
+}
+
+void gripEmptyTea(){ //Good
+    int speed = 2;
+    float target[NUM_SERVOS] = {.4, .65, .65, .65, .65};
+    float threshold = .6;
+    execTestCase(target, speed, threshold, "Grip Empty Tea");
+}
+
+void crushEmptyTea(){
+    int speed = 2;
+    float target[NUM_SERVOS] = {.4, .9, .9, .9, .9};
+    float threshold = 3;
+    execTestCase(target, speed, threshold, "Crush Empty Tea");
+}
+
+//t_start is the time that the test case was started
+void printSensorReading(float voltages[NUM_SERVOS], unsigned long t_start){
+    unsigned long curr_t = millis();
+    unsigned long relative_t = curr_t - t_start;
+    Serial.print(relative_t);
+    for(int i = 0; i < NUM_SERVOS; i++){
+        Serial.print(",");
+        Serial.print(voltages[i]);
+    }
+    Serial.print("\n");
+}
+
+void execTestCase(float target[NUM_SERVOS], int speed, float threshold, String test_case_name){
+    float voltages[NUM_SERVOS];
+    int result[NUM_SERVOS];
+    unsigned long t_start = millis();
+    Serial.print("Test Case: " + test_case_name + "\n");
+    Serial.print("Threshold: ");
+    Serial.print(threshold);
+    Serial.print("\n");
     while(1){
         if(checkReset()){
             return;
         }
 
         sensor_control->readAll(voltages);
-        Serial.print("Thumb: ");
-        Serial.print(voltages[0]);
-        Serial.print("\nRing: ");
-        Serial.print(voltages[3]);
-        Serial.print("\n");
+        printSensorReading(voltages, t_start);
         for(int i = 0; i < NUM_SENSORS; i++){
             if(voltages[i] >= threshold){
-                Serial.print("Threshold reached on ");
-                Serial.print(i);
-                Serial.print(" Value=");
-                Serial.print(voltages[i]);
-                Serial.print("\n");
                 return;
             }
         }
         servo_control->gripTo(target, speed, result);
     }
 }
-
-void readSensors(){
-    while(1){
-        float voltages[NUM_SERVOS];
-        sensor_control->readAll(voltages);
-        Serial.print(voltages[3]);
-        Serial.print("\n");
-    }
-}
-
-
-
 
 void execCommand(String &cmd){
     if(cmd == "openclose"){
@@ -141,14 +221,51 @@ void execCommand(String &cmd){
     else if(cmd == "reset"){
         reset();
     }
-    else if(cmd == "gripcoffee"){
-        gripCoffee();
+    else if(cmd == "gripCoke"){
+        gripCoke();
     }
-    else if(cmd == "readsensors"){
-        readSensors();
+    else if(cmd == "gripFullTea"){
+        gripFullTea();
     }
-    else if(cmd == "Mario"){
+    else if(cmd == "gripEmptyTea"){
+        gripEmptyTea();
+    }
+    else if(cmd == "crushEmptyTea"){
+        crushEmptyTea();
+    }
+    else if(cmd == "gripWater"){
+        gripWater();
+    }
+    else if(cmd == "gripEraser"){
+        gripEraser();
+    }
+    else if(cmd == "gripThermos"){
+        gripThermos();
+    }
+    else if(cmd == "gripFullWater"){
+        gripFullWater();
+    }
+    else if(cmd == "downPinky"){
+        downFinger(4);
+    }
+    else if(cmd == "downRing"){
+        downFinger(3);
+    }
+    else if(cmd == "downMiddle"){
+        downFinger(2);
+    }
+    else if(cmd == "downIndex"){
+        downFinger(1);
+    }
+    else if(cmd == "downThumb"){
+        downFinger(0);
+    }
+    /*Easter Eggs ;) */
+    else if(cmd == "mario"){
         mario();
+    }
+    else if(cmd == "lobo"){
+        lobo();
     }
 }
 
@@ -158,80 +275,4 @@ void loop() {
         execCommand(cmd);
     }
     return;
-/*
-        if(cmd == "stop"){
-            reached_target_voltage = 1;
-        }
-        if(cmd == "start"){
-            reached_target_voltage = 0;
-        }
-        if(cmd == "reset"){
-            while(1){
-                float target[NUM_SERVOS] = {.3, 0, 0, 0, 0};
-                int result[NUM_SERVOS];
-                servo_control->gripTo(target, 2, result);
-            }
-        }
-*/
-    int speed = 1;
-    //Read all of the sensors, and if any of them are > target voltage, stop
-    float result[NUM_SENSORS];
-    sensor_control->readAll(result);
-    int changed = 0;
-    //Serial.println(result[4]);
-    for(int i = 0; i < NUM_SENSORS; i++){
-        /*
-        if(result[i] >= target_voltage){
-            reached_target_voltage = 1;
-        }
-        */
-    }
-    if(result[0] >= target_voltage){
-        reached_target_voltage = 1;
-    }
-    Serial.println(result[0]);
-    /*
-    if(changed){
-        reached_target_voltage = 1;
-    }
-    else{
-        reached_target_voltage = 0;
-    }
-    */
-
-    /*
-    String cmd;
-    if(serial_comm->getCmdIfAvailable(cmd) != -1){
-        if(cmd == "stop"){
-            reached_target_voltage = 1;
-        }
-        if(cmd == "start"){
-            reached_target_voltage = 0;
-        }
-        if(cmd == "reset"){
-            while(1){
-                float target[NUM_SERVOS] = {.3, 0, 0, 0, 0};
-                int result[NUM_SERVOS];
-                servo_control->gripTo(target, 2, result);
-            }
-        }
-    }
-    */
-
-        /*
-    if(!reached_target_voltage){
-        int result[NUM_SERVOS];
-        servo_control->gripTo(target, speed, result);
-        for(int i = 1; i < NUM_SERVOS; i++){
-            if(result[i]){
-                if(target[i] == .7){
-                    target[i] = 0;
-                }
-                else {
-                    target[i] = .7;
-                }
-            }
-        }
-    }
-        */
 }
